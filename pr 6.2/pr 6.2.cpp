df5ca6821fd4fe2fa2f93e7f54bb41f325e45954
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:05:29 +0300

    Add files via upload

---
+++ pr 6.2/pr 6.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(0, "ru");
+    int number = 7;
+
+    std::cout << "Таблица умножения на " << number << ":\n";
+
+    for (int i = 2; i <= 9; i++) {
+        int result = number * i;
+        std::cout << number << " x " << i << " = " << result << std::endl;
+    }
+
+    return 0;
+
+}
